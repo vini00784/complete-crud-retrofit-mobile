84c7eb1377603800a15ddaeced3bdc0112435c8e
vini00784 <vo70346@gmail.com>
2023-03-06 09:42:54 -0300

    file with standard and fixed messages

---
+++ app/src/main/java/br/senai/sp/jandira/contactretrofit/Constants/Constants.kt
@@ -0,0 +1,9 @@
+package br.senai.sp.jandira.contactretrofit.Constants
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "http://10.107.144.4:8080/api/"
+        const val CONTENT_TYPE = "application/json"
+    }
+}
